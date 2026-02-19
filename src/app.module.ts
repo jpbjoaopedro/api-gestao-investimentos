a5a0d4689ff0c0a4115d9a1db5e11f89fc961b54
João Pedro Bandeira <jpbandeiradossantos@gmail.com>
2026-02-19 14:24:09 -0300

    feat: Set up NestJS configuration module with Joi validation for environment variables.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './database/prisma.module';
+import { ConfigModule } from '@nestjs/config';
+import { envValidationSchema } from './config/env.validation';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: envValidationSchema
+    })
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+  ],
 })
 export class AppModule { }
