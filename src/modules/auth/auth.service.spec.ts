50be0d39d4d71aeba56a6b34032223c10046eab2
João Pedro Bandeira <jpbandeiradossantos@gmail.com>
2026-03-04 14:38:36 -0300

    test: Enhance AuthService tests with mock dependencies and user registration scenarios

--- src/modules/auth/auth.service.spec.ts
+++ src/modules/auth/auth.service.spec.ts
@@ -1,18 +1,61 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { PrismaService } from '../../database/prisma.service';
+import { JwtService } from '@nestjs/jwt';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let prisma: PrismaService;
+
+  const mockPrisma = {
+    user: {
+      findUnique: jest.fn(),
+      create: jest.fn(),
+      update: jest.fn(),
+    },
+  };
+
+  const mockJwt = {
+    signAsync: jest.fn().mockResolvedValue('mockToken'),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        { provide: PrismaService, useValue: mockPrisma },
+        { provide: JwtService, useValue: mockJwt }
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
+    prisma = module.get<PrismaService>(PrismaService)
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('should register a new user', async () => {
+    mockPrisma.user.findUnique.mockResolvedValue(null);
+    mockPrisma.user.create.mockResolvedValue({
+      id: '1',
+      email: 'test@test.com',
+    });
+
+    const result = await service.register({
+      email: 'test@test.com',
+      password: '123456',
+    });
+
+    expect(result).toHaveProperty('accessToken');
+    expect(result).toHaveProperty('refreshToken');
   });
+
+  it('should throw if email alredy exists', () => {
+    mockPrisma.user.findUnique.mockResolvedValue({ id: 1 });
+
+    expect(
+      service.register({
+        email: 'test@test.com',
+        password: '123456'
+      })
+    ).rejects.toThrow();
+  })
 });
